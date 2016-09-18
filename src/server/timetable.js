c5dd9ce3d377e669d23d7dbaa926ba2f34dd844a
Alexander Tsoi <alexander.tsoi@outlook.com>
2016-09-17 22:10:55 -0400

    Add timetable service

--- src/server/timetable.js
+++ src/server/timetable.js
@@ -8,19 +8,58 @@
         module.exports = factory(require('babyparse'));
     } else {
         // Browser globals (Note: root is window)
-        root.returnExports = factory(root.babyparse);
+        root.Timetable = factory(root.Baby);
     }
 } (this, function (baby) {
     var state = {};
 
-    console.log("babyparse", baby);
-    console.log("babyparse.Baby", baby.Baby)
-
     function loadData(stops, stop_times, trips, routes) {
-        state.stops = baby.parse(stops);
-        state.stop_times = baby.parse(stop_times);
-        state.trips = baby.parse(trips);
-        state.routes = baby.parse(routes);
+        var i, options = { header: true, dynamicTyping: true };
+
+        // stops
+        state.stops = baby.parse(stops, options).data;
+
+        // stop times
+        state.stop_times = baby.parse(stop_times, options).data;
+
+        // trips
+        var trips = baby.parse(trips, options).data;
+        var tripMap = {}, trip;
+        for (i = 0, len = trips.length; i < len; i++) {
+            trip = trips[i];
+            tripMap[trip.trip_id] = trip;
+        }
+        state.trips = tripMap;
+
+        // routes
+        var routes = baby.parse(routes, options).data;
+        var routeMap = {}, route;
+        for (i = 0, len = routes.length; i < len; i++) {
+            route = routes[i];
+            routeMap[route.route_id] = route;
+        }
+        state.routes = routeMap;
+    }
+
+    function getNextRoutes(lat, lon, count, now) {
+        // defaults
+        count = count || 5;
+        now = now || new Date();
+
+        var now_time = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+        var stop = getClosestStop(lat, lon, state.stops);
+        var times = getNextTimes(stop.stop_id, now_time, state.stop_times, count);
+        var result = [], time, trip;
+        for (var i = 0, len = times.length; i < len; i++) {
+            time = times[i];
+            trip = state.trips[time.trip_id];
+            result.push({
+                stop_time: time,
+                trip: state.trips[time.trip_id],
+                route: state.routes[trip.route_id]
+            })
+        }
+        return result;
     }
 
     function getDistance(latitude1, longitude1, latitude2, longitude2) {
@@ -40,12 +79,7 @@
     }
 
     function getRad(value) {
-         return value * Math.PI / 180;
-    }
-
-    function getNextRoutes(lat, lon, count, now) {
-        var stop = getClosestStop(lat, lon, state.stops);
-        return stop;
+        return value * Math.PI / 180;
     }
 
     function getClosestStop(lat, lon, stops) {
@@ -54,14 +88,49 @@
             distance = getDistance(lat, lon, stops[i].stop_lat, stops[i].stop_lon);
             if (distance < closestDistance) {
                 closestDistance = distance;
-                closestStop = stop[i];
+                closestStop = stops[i];
             }
         }
         return closestStop;
     }
 
+    function getNextTimes(stop_id, now_time, stop_times, count) {
+        var i, stop_time, prev_stop_time, start_index = -1, result = [];
+        for (i = 0, len = stop_times.length; i < len; i++) {
+            stop_time = stop_times[i];
+            if (stop_time.stop_id === stop_id) {
+                result.push(stop_time);
+            }
+        }
+
+        result.sort(function (a, b) {
+            return parseTime(a.arrival_time) - parseTime(b.arrival_time);
+        });
+
+        for (i = 0, len = result.length; i < len; i++) {
+            if (now_time > parseTime(result[i].arrival_time)) {
+                if (start_index < 0) {
+                    start_index = Math.max(0, i - 1);
+                }
+            }
+        }
+
+        if (start_index > 0) {
+            result.splice(0, start_index);
+        }
+        result.splice(count);
+        return result;
+    }
+
+    function parseTime(time) {
+        if (time === undefined) return 0;
+        var m = time.match(/(\d+)\:(\d+)\:(\d+)/i);
+        return parseInt(m[1], 10) * 3600 + parseInt(m[2], 10) * 60 + parseInt(m[3], 10);
+    }
+
     // Exposed public methods
     return {
-        loadData: loadData
+        loadData: loadData,
+        getNextRoutes: getNextRoutes
     }
 }));
