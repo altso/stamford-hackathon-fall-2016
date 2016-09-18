f9017d3ea122ed7f7b935fde1820f4cc334d94ff
Roman Presnakov <roman.presnakov@gmail.com>
2016-09-17 23:15:44 -0400

    added webservice call

--- blippar/javascript/main.js
+++ blippar/javascript/main.js
@@ -9,14 +9,8 @@ scene.onCreate = function() {
     var Plane = scene.addSprite().setColor('#ffff00').setScale(500);
 	scene.addText('Test1')
 		 .setFontSize(72)
-		 .setTranslationY(400);    	
+		 .setTranslationY(400);
 
-}
-
-scene.onShow = function()
-{
-    console.log("Scene on show");
-	
 	var logo = scene.addSprite("15.jpg")
 					.setName("logo")
 					.setTranslation(0, 0, 1);
@@ -26,6 +20,17 @@ scene.onShow = function()
 		animateLogo(this);
 	}
 
+	logo.onTouchEnd = function(id, x, y)
+	{
+    	PngWebServiceCall(Math.round(Math.random() * 10) + 1, ParseResult);
+	    console.log("Touched on : " + this.getName());
+	}
+}
+
+scene.onShow = function()
+{
+    console.log("Scene on show");
+	
 	var lat = blipp.getGeo().getLat();
 	var lon = blipp.getGeo().getLon();
 	console.log("Our coordinates: lat=" + lat + ", lon=" + lon);
@@ -40,3 +45,29 @@ animateLogo = function(node)
 		return Math.sqrt(value);
 	}
 }
+
+PngWebServiceCall = function(value, callback) {
+	blipp.downloadAssets('https://api.scriptrapps.io/test.png?value=' + value,
+	['test.png'],
+	'get',
+	function (status, info) {
+		loaded = true;
+		if (status == 'OK') {
+			console.log('Download Done');
+			var json = blipp.loadJson('test.png', true);
+			console.log("JSON: " + json);
+			console.log(json.response.result.data);
+			callback(json);
+		} else {
+			console.log('Loaded ' + info + ' %');
+		}
+	},
+	['Authorization', 'bearer UDc4QTA0NTdDMDpzY3JpcHRyOjk3RjU1MTFCRUUwMDQ0RTk0OUU1NEIwMUJBQjE0ODJE'],
+	 true);	
+}
+
+ParseResult = function(json) {
+	scene.addText(json.response.result.data)
+		 .setFontSize(72)
+		 .setTranslationY(-500);  	
+} 
